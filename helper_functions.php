21e9e2248c3d441321f93f6ccb1fd67f9bd38eab
Holkan <luah613@gmail.com>
2019-04-04 11:06:59 -0600

    default tag taxonomy

--- helper_functions.php
+++ helper_functions.php
@@ -28,19 +28,18 @@
 	function co_get_term_taxonomy($barra_name, $input_name){
 		global $post;
 		$options = get_option($barra_name);
-		$args = array(
-			'taxonomy'=>'hashtag',
-			'hide_empty'=>false
-		);
+		$args = array('taxonomy'=>'post_tag','hide_empty'=>false,'orderby'=>'count','order'=>'DESC');
 		$terms = get_terms($args);
 		if(is_array($terms)&&!empty($terms)): ?>
 			<select name='<?php echo $barra_name."[$input_name]"; ?>' class="custom_input">
 				<option value="">Empty Field</option>
 				<?php 
 					foreach ($terms as $term): 
-						$is_selected = selected($term->term_id, $options[$input_name], false); ?>
-						<option value="<?php echo esc_attr($term->term_id); ?>" <?php echo $is_selected;?>><?php echo esc_html($term->name); ?></option>
+						if($term->count >= 9):
+							$is_selected = selected($term->term_id, $options[$input_name], false); ?>
+							<option value="<?php echo esc_attr($term->term_id); ?>" <?php echo $is_selected;?>><?php echo esc_html($term->name); ?></option>
 				<?php 
+						endif;
 					endforeach; ?>
 			</select>
 	<?php
@@ -50,7 +49,7 @@
 	function co_get_tema_posts($barra_name, $input_name, $term_id){
 		global $post;
 		$options = get_option($barra_name);
-		$args = array('post_type'=>'post','posts_per_page'=>10,'post_status'=>'publish','orderby'=>'date','order'=>'DESC','tax_query'=>array(array('taxonomy'=>'hashtag','field'=>'term_id','terms'=>array($term_id))));
+		$args = array('post_type'=>'post','posts_per_page'=>10,'post_status'=>'publish','orderby'=>'date','order'=>'DESC','tax_query'=>array(array('taxonomy'=>'post_tag','field'=>'term_id','terms'=>array($term_id))));
 		$posts = new WP_Query($args);
 		if($posts->have_posts()): ?>
 			<select name='<?php echo $barra_name."[$input_name]"; ?>' class="custom_input">
